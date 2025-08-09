1de11f2b1d2908755926198bee0f52c12f7755e1
Nilesh Patil <nily0529@gmail.com>
2025-08-09 21:25:22 +0530

    dist changes from FE to BE

--- hotel-nilanjan-FE/src/environments/environment.prod.ts
+++ hotel-nilanjan-FE/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://192.168.1.107'
-  // baseUrl: 'http://raspberrypi:5000' 
+  // baseUrl: 'http://192.168.1.107'
+  baseUrl: 'http://raspberrypi:5000'
 };
